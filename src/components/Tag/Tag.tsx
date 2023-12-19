5e922a3471b840a5d580e3f4e3d5aec96f07af1a
Wenhan Lyu <lvwh0330@gmail.com>
2023-12-18 23:17:00 -0500

    fix: Tags styles update

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -12,23 +12,36 @@ export const Tag = (prop: TagProps) => {
       <>
         {type === 'section' &&
             <div
-                className="inline-flex items-center justify-center bg-merino p-2.5 h-[33px] text-nero uppercase text-lg font-semibold leading-[120%] tracking-[0.36px]"
-                style={{fontSize: '18px', lineHeight: '120%', letterSpacing: '0.36px'}}>
+                className="inline-flex items-center justify-center bg-merino p-2.5 h-[33px] text-nero uppercase leading-[120%] tracking-[0.36px]"
+                style={{
+                  fontSize: '18px',
+                  lineHeight: '120%',
+                  letterSpacing: '0.36px',
+                  fontFamily: '"Press Start 2P", cursive'
+                }}>
               {text}
             </div>
 
         }
         {type === 'relevance' &&
             <div
-                className="inline-flex items-center justify-center bg-merino p-[5px] h-[19px] text-nero uppercase text-lg font-semibold leading-[120%] tracking-[0.36px] border-solid border-nero border-[1px]"
-                style={{fontSize: '18px', lineHeight: '120%', letterSpacing: '0.36px'}}>
+                className="inline-flex items-center justify-center bg-merino p-[5px] h-[19px] text-nero uppercase leading-[120%] tracking-[0.36px] border-solid border-nero border-[1px]"
+                style={{
+                  fontSize: '18px',
+                  lineHeight: '120%',
+                  letterSpacing: '0.36px',
+                }}>
               {text}
             </div>
         }
         {type === 'more' &&
             <div
-                className="inline-flex items-center justify-center bg-merino p-[5px] h-[19px] text-nero uppercase text-lg font-semibold leading-[120%] tracking-[0.36px] border-solid border-nero border-[1px]"
-                style={{fontSize: '18px', lineHeight: '120%', letterSpacing: '0.36px'}}>
+                className="inline-flex items-center justify-center bg-merino p-[5px] h-[19px] text-nero uppercase leading-[120%] tracking-[0.36px] border-solid border-nero border-[1px]"
+                style={{
+                  fontSize: '18px',
+                  lineHeight: '120%',
+                  letterSpacing: '0.36px',
+                }}>
               {numberOfMore}+
             </div>
         }
